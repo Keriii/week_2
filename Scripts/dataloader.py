433028e39243972c5dec1d59cbce392bca90ce41
Kerod Sisay <Kerod53@gmail.com>
2023-12-22 01:37:55 +0300

    loaded into pg db

--- Scripts/dataloader.py
+++ Scripts/dataloader.py
@@ -23,14 +23,14 @@ def create_engines():
     """
     creates engine using sqlalchemy for a given paramters:
     """
-    engine = create_engine("postgresql://postgres:kerod53@@localhost:5432/Sensor_data")
+    engine = create_engine("postgresql://postgres:kerod53@localhost:5432/Sensor_data")
     return engine
 
-def write_dataframe_to_table(df: pd.DataFrame, table_name: str)->None:
+def write_dataframe_to_table(df: pd.DataFrame, table_name: str):
     """
     Writes a pandas dataframe to a new table in the PostgreSQL database.
     """
-    engine = create_engine("postgresql://postgres:kerod53@@localhost:5432/Sensor_data")
+    engine = create_engine("postgresql://postgres:kerod53@localhost:5432/Sensor_data")
 
     df.to_sql(table_name, engine, index=False, if_exists='replace')
     print(f"Dataframe successfully written to the '{table_name}' table.")
